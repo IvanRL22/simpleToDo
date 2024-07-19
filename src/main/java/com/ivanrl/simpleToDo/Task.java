4bcdf6959853eac12b37c28e74a71aa3fced5537
Iván Recasens <irecasensl@gmail.com>
2024-07-19 21:17:36 +0200

    Link tasks to user

--- src/main/java/com/ivanrl/simpleToDo/Task.java
+++ src/main/java/com/ivanrl/simpleToDo/Task.java
@@ -1,6 +1,7 @@
 package com.ivanrl.simpleToDo;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,8 +10,9 @@ import lombok.Setter;
 @Table(name = "TASKS")
 @Getter
 @Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Task {
+
     @Id
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,9 +21,12 @@ public class Task {
     private String name;
     @Column(name = "DONE")
     private boolean done;
+    @Column(name = "USERNAME", nullable = false, updatable = false)
+    private String username; // TODO Eventually this should be a custom entity
 
-    public Task(String name) {
+    public Task(String name, String username) {
         this.name = name;
+        this.username = username;
         this.done = false;
     }
 
--- src/main/java/com/ivanrl/simpleToDo/controller/TaskController.java
+++ src/main/java/com/ivanrl/simpleToDo/controller/TaskController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -23,35 +25,39 @@ public class TaskController {
     private final TaskRepository taskRepository;
 
     @GetMapping(headers = "HX-Request")
-    public String tasks(Model model) {
-        model.addAttribute("tasks", this.taskRepository.findAllByDone(false));
+    public String tasks(@AuthenticationPrincipal UserDetails user,
+                        Model model) {
+
+        model.addAttribute("tasks", this.taskRepository.findAllByDoneAndUsername(false, user.getUsername()));
 
         return "tasks :: #content";
     }
 
     @PostMapping(value = "/add", headers = "HX-Request")
     public String addTask(NewTask newTask,
+                          @AuthenticationPrincipal UserDetails user,
                           Model model) {
-        this.taskRepository.save(new Task(newTask.name()));
+        this.taskRepository.save(new Task(newTask.name(), user.getUsername()));
 
-        model.addAttribute("tasks", this.taskRepository.findAllByDone(false));
+        model.addAttribute("tasks", this.taskRepository.findAllByDoneAndUsername(false, user.getUsername()));
 
         return FRAGMENT_TASKS_TASKLIST;
     }
 
     @PatchMapping(value = "/complete/{id}", headers = "HX-Request")
     public String completeTask(@PathVariable Long id,
+                               @AuthenticationPrincipal UserDetails user,
                                Model model) {
         this.taskRepository.completeTask(id);
 
-        model.addAttribute("tasks", this.taskRepository.findAllByDone(false));
+        model.addAttribute("tasks", this.taskRepository.findAllByDoneAndUsername(false, user.getUsername()));
 
         return FRAGMENT_TASKS_TASKLIST;
     }
 }
 
 interface TaskRepository extends JpaRepository<Task, Long> {
-    Collection<Task> findAllByDone(boolean done);
+    Collection<Task> findAllByDoneAndUsername(boolean done, String username);
 
     @Transactional
     @Modifying
